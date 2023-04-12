513d7a2d9255e9a69e88083ddb8e1069b4bd6070
Diego G <Diego93gonzo@gmail.com>
2023-04-12 13:05:48 -0400

    Updates

---
+++ Morning_Algo.js
@@ -0,0 +1,44 @@
+//
+
+let countPositives = 0;
+let numbers = [3, 4, -2, 7, 16, -8, 0];
+// var arr1 = [3, 4, -2, 7, 16, -8, 0]
+
+// function morningStart(){
+  for(var i=0; i<numbers.length; i++){
+    if(numbers[i] > 0){
+      countPositives++;
+    }
+  }
+//   return countPositives
+// }
+
+// function morningStart(arr){
+//   for(var i=0; i<arr.length; i++){
+//     if(arr[i] > 0){
+//       newArr.push(arr[i])
+//     }
+//   }
+//   return newArr
+// }
+
+
+// How do we check if it's positive? 
+// We can check if the values are greater than 0. 
+
+// We'll need a loop to go through the array.
+// We'll need a conditional to check the value for if it's positive.
+
+// morningStart(arr1);
+
+console.log("there are " + countPositives + " positive values");
+
+
+/*
+
+Bonus Challenge: 
+Level 1: Write this as a function that accepts arrays as parameters.
+Level 2: Include a return statement that gives back the counted positive numbers.
+Level 3: Instead of returning the count, return a new array of only the positive numbers.
+
+*/
