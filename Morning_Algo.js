87899d3dc0aa17dd9d519897bcd9d29d996cd6a0
Diego G <Diego93gonzo@gmail.com>
2023-04-14 19:07:52 -0400

    ProfilePage updates

--- Morning_Algo.js
+++ Morning_Algo.js
@@ -1,18 +1,49 @@
+/*
+More info about Array Functions!
+
+Add a value to the end of an array.
+push(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+
+Remove the value at the end of the array (returns the popped value!)
+pop(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop
+
+Add a value to the front of an array.
+unshift(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
+
+Remove the value at the front of an array (returns the shifted value!)
+shift(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
+
+*/
+
+
+// Write a function to reverse the values in a given array.
+
+// Build your own array lists as a group, so share some of your favorite movies, games, or even pokemon! (Or make up your own!)
+var favMovies = [];
+var favGames = [];
+var favPokemon = [];
+
+var boringArr = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+// var boringTemp = boringArr[2]
 
 
 
-function isPal(arr) {
-  for(var left=0; left<arr.length/2; left++) {
-      var right = arr.length-1-left;
-      if(arr[left] != arr[right]) {
-          return "Not a pal-indrome!";
-      }
-  }
-  return "Pal-indrome!";
-}
-  
-var result1 = isPal( [1, 1, 2, 2, 1] );
-console.log(result1);
-  
-var result2 = isPal( [3, 2, 1, 1, 2, 3] );
-console.log(result2);
+// function reverseArr(arr) {
+//   var temp = "";
+//   for ( var i = arr.length - 1; i > 0; i++) {
+
+//   }
+
+// }
+
+var R2 = boringArr.reverse()
+console.log(R2)
+
+// reverseArr(boringArr);
+
+// Optional Challenges!
+// Level 1: Construct the function with both a For Loop and then a While Loop. (Watch out for infinite loops!)
+// Level 2: Knowing .pop() RETURNS the removed value, find a way to use this to move a value to the other side.
+// Level 3: Create the function in a way that reverses the Array "in place" where you don't create a new Array and use a temp variable!
+// (Hint: This one can be challenging, so don't stress this one!)
