612db7b2c219f502c6fd4cdfce294cbbb1806fef
Diego Gonzalez <130378289+GuyTooChill@users.noreply.github.com>
2023-04-18 14:46:53 -0400

    Add files via upload

---
+++ Likes/script.js
@@ -0,0 +1,14 @@
+
+function changeLikes() {
+    let counter = document.querySelector('#counter');
+    counter.innerText++
+}
+function changeLikes1() {
+    let counter = document.querySelector('#counter1');
+    counter.innerText++
+}
+function changeLikes2() {
+    let counter = document.querySelector('#counter2');
+    counter.innerText++
+}
+
